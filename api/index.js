326a34c2b12d14b2dbadb3bc1a8cfe345d1ade28
Paulo-Rafael1971 <carecaipa@gmail.com>
2025-04-26 10:48:05 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const analyze = require('./analyze');
 
 const app = express();
 app.use(bodyParser.json());
+
+// Servir arquivos estáticos da pasta public
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Rota API
 app.use('/api', analyze);
 
+// Se alguém acessar '/', entrega o index.html da public
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/../public/index.html');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = app;
